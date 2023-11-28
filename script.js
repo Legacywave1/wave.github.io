246cce89497adabe96b70f4d7a0329c17ba50e1b
Legacywave1 <105254642+Legacywave1@users.noreply.github.com>
2023-11-28 02:36:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const words = ["Welcome", "to", "the", "wave", "world"];
 let index = 0;
 
 function displayWord() {
-    if (index < words.length) {
-        document.getElementById('welcome-text').innerHTML += words[index] + " ";
-        index++;
-        setTimeout(displayWord, 500); // Adjust the typing speed (in milliseconds) here
-    } else {
-        setTimeout(changeBackground, 2000); // Change the background after the typing is done
-    }
+  if (index < words.length) {
+    document.getElementById('welcome-text').innerHTML += words[index] + " ";
+    index++;
+    setTimeout(displayWord, 500); // Adjust the typing speed (in milliseconds) here
+  } else {
+    setTimeout(changeBackground, 2000); // Change the background after the typing is done
+  }
 }
 
 function changeBackground() {
-    document.querySelector('.stars').style.background = "url('data analytics.jpg')"; // Replace with the path to your data analysis image
-    document.querySelector('.centered-text').innerHTML = "<h1>Page under maintenance</h1>";
-    // You can add the machine learning image here as well
+  document.querySelector('.stars').style.background = "url('path_to_data_analysis_image.jpg')"; // Replace with the path to your data analysis image
+  document.querySelector('.centered-text').innerHTML = "<h1>Page under maintenance</h1>";
+  // You can add the machine learning image here as well
 }
 
 displayWord();
