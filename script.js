f7e40194bf97c96c0c9f178d837f14137c4e53bc
Wave <cypriananku121@gmail.com>
2023-11-28 02:29:55 +0000

    Added the images

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function displayWord() {
 }
 
 function changeBackground() {
-    document.querySelector('.stars').style.background = "url('C:\Users\Wave\Desktop\Data analysis\Data analytics.webp')"; // Replace with the path to your data analysis image
+    document.querySelector('.stars').style.background = "url('data analytics.jpg')"; // Replace with the path to your data analysis image
     document.querySelector('.centered-text').innerHTML = "<h1>Page under maintenance</h1>";
     // You can add the machine learning image here as well
 }
