2e6cfcf4b87b4addf7463ee41e8820b5106b6a97
Wave <cypriananku121@gmail.com>
2023-11-28 02:11:07 +0000

    Updating the website

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let index = 0;
 
 function displayWord() {
     if (index < words.length) {
-        document.querySelector('.centered-text').innerHTML += "<h1>" + words[index] + "</h1>";
+        document.getElementById('welcome-text').innerHTML += words[index] + " ";
         index++;
-        setTimeout(displayWord, 1000); // Change the delay (in milliseconds) here if you want the words to appear faster or slower
+        setTimeout(displayWord, 500); // Adjust the typing speed (in milliseconds) here
+    } else {
+        setTimeout(changeBackground, 2000); // Change the background after the typing is done
     }
+}
+
+function changeBackground() {
+    document.querySelector('.stars').style.background = "url('data_analysis_image.jpg')"; // Replace with the path to your data analysis image
+    document.querySelector('.centered-text').innerHTML = "<h1>Page under maintenance</h1>";
+    // You can add the machine learning image here as well
 }
 
 displayWord();
